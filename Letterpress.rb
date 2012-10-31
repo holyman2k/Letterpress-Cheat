82e4a5978ce27b286f42fd38b5b465a44b3697a7
Charlie Wu <charlie.y.wu@gmail.com>
2012-10-31 14:39:19 +1100

    fixed min length bug

--- Letterpress.rb
+++ Letterpress.rb
@@ -2,8 +2,8 @@ load 'LetterService.rb'
 load 'Word.rb'
 load 'WordMatcher.rb'
 
-letters = "fohvlmwybhdoxmvrdllnvityt"
-mustHaveLetters = "vx"
+letters = "tmtlxqsxattpbfeijheonlcms"
+mustHaveLetters = "lhf"
 minLength = 3;
 
 
@@ -15,32 +15,28 @@ Dir.open(dicpath).each{ |file|
 
     File.open(File.join(dicpath, file)).each{ |word|
         begin
-            wordlist.push Word.new word.strip if word.length => minLength and /[']/.match(word) == nil
+            wordlist.push Word.new word.strip if word.length > minLength and /[']/.match(word) == nil
         rescue
             errors.push "errored words #{word}"
         end
     } if !/^\./.match file
 }
 
-wordlist.each {|word|
-    puts word.value
+matchList = []
+
+# create list of words can be made from letters
+wordlist.each { |word|
+
+    matchList.push word if Match.matchWord word, letterMap
 }
 
-# matchList = []
+# filter out using must have words
+Match.filterWord! matchList, LetterService.to_letters(mustHaveLetters) if mustHaveLetters.length > 0
 
-# # create list of words can be made from letters
-# wordlist.each { |word|
+#sort words by length
+matchList.sort! {|a, b| a.value.length <=> b.value.length}
 
-#     matchList.push word if Match.matchWord word, letterMap
-# }
-
-# # filter out using must have words
-# Match.filterWord! matchList, LetterService.to_letters(mustHaveLetters) if mustHaveLetters.length > 0
-
-# #sort words by length
-# matchList.sort! {|a, b| a.value.length <=> b.value.length}
-
-# output
-# matchList.each { |word|
-#     puts word.value
-# }
+#output
+matchList.each { |word|
+    puts word.value
+}
